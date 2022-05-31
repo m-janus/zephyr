33473b6c78f37daf3fc1a6d7bba93060a1c208aa
Marek Janus <marek.janus@grinn-global.com>
2022-05-31 13:17:09 +0200

    drivers: led: lp503x: extend driver with support for LP5009/LP5012
    This modification extends this driver to handle also LP5009 and LP5012
    devices.
    Add handling for EN pin in driver.
    Replace partial manual reconfiguration of device's registers at its
    initialization with usage of dedicated reset functionality.
    
    Signed-off-by: Marek Janus <marek.janus@grinn-global.com>

--- drivers/led/lp5009.c
+++ drivers/led/lp5009.c
@@ -4,51 +4,37 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-#define DT_DRV_COMPAT ti_lp503x
+#define DT_DRV_COMPAT ti_lp5009
 
 #include "lp50xx.h"
 #include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(lp503x, CONFIG_LED_LOG_LEVEL);
+LOG_MODULE_REGISTER(lp5009, CONFIG_LED_LOG_LEVEL);
 
-#define LP503X_LED_CONFIG0		0x2
+#define LP5009_LED_CONFIG0		0x2
 #define   CONFIG0_LED0_BANK_EN		BIT(0)
 #define   CONFIG0_LED1_BANK_EN		BIT(1)
 #define   CONFIG0_LED2_BANK_EN		BIT(2)
 #define   CONFIG0_LED3_BANK_EN		BIT(3)
-#define   CONFIG0_LED4_BANK_EN		BIT(4)
-#define   CONFIG0_LED5_BANK_EN		BIT(5)
-#define   CONFIG0_LED6_BANK_EN		BIT(6)
-#define   CONFIG0_LED7_BANK_EN		BIT(7)
 
-#define LP503X_LED_CONFIG1		0x3
-#define   CONFIG1_LED8_BANK_EN		BIT(0)
-#define   CONFIG1_LED9_BANK_EN		BIT(1)
-#define   CONFIG1_LED10_BANK_EN		BIT(2)
-#define   CONFIG1_LED11_BANK_EN		BIT(3)
+#define LP5009_DEVICE_RESET		0x17
 
-#define LP503X_DEVICE_RESET		0x38
+#define LP5009_BANK_BRIGHTNESS		0x3
+#define LP5009_BANK_A_COLOR		0x4
+#define LP5009_BANK_B_COLOR		0x5
+#define LP5009_BANK_C_COLOR		0x6
 
-#define LP503X_BANK_BRIGHTNESS		0x4
-#define LP503X_BANK_A_COLOR		0x5
-#define LP503X_BANK_B_COLOR		0x6
-#define LP503X_BANK_C_COLOR		0x7
-
-#define LP503X_LED_BRIGHTNESS_BASE	0x8
-#define LP503X_OUT_COLOR_BASE		0x14
-#define LP503X_MAX_LEDS			12
-/*
- * LED channels mapping.
- */
-#define LP503X_NUM_CHANNELS		52
+#define LP5009_LED_BRIGHTNESS_BASE	0x7
+#define LP5009_OUT_COLOR_BASE		0xB
+#define LP5009_MAX_LEDS			4
+#define LP5009_NUM_CHANNELS		20
 
 /* LED color channels. */
-#define LP503X_LED_COL_CHAN_BASE	16
+#define LP5009_LED_COL_CHAN_BASE	8
 
-#define LP503X_CHANNEL_BASE		LP503X_BANK_BRIGHTNESS
+#define LP5009_CHANNEL_BASE		LP5009_BANK_BRIGHTNESS
 
-
-static int lp503x_set_brightness(const struct device *dev,
+static int lp5009_set_brightness(const struct device *dev,
 				 uint32_t led, uint8_t value)
 {
 	const struct lp50xx_config *config = dev->config;
@@ -59,13 +45,13 @@ static int lp503x_set_brightness(const struct device *dev,
 		return -EINVAL;
 	}
 
-	buf[0] = LP503X_LED_BRIGHTNESS_BASE + led_info->index;
+	buf[0] = LP5009_LED_BRIGHTNESS_BASE + led_info->index;
 	buf[1] = (value * 0xff) / 100;
 
 	return i2c_write_dt(&config->bus, buf, sizeof(buf));
 }
 
-static int lp503x_set_color(const struct device *dev, uint32_t led,
+static int lp5009_set_color(const struct device *dev, uint32_t led,
 			    uint8_t num_colors, const uint8_t *color)
 {
 	const struct lp50xx_config *config = dev->config;
@@ -76,7 +62,7 @@ static int lp503x_set_color(const struct device *dev, uint32_t led,
 		return -EINVAL;
 	}
 
-	buf[0] = LP503X_OUT_COLOR_BASE + 3 * led_info->index;
+	buf[0] = LP5009_OUT_COLOR_BASE + 3 * led_info->index;
 	buf[1] = color[0];
 	buf[2] = color[1];
 	buf[3] = color[2];
@@ -84,15 +70,15 @@ static int lp503x_set_color(const struct device *dev, uint32_t led,
 	return i2c_write_dt(&config->bus, buf, sizeof(buf));
 }
 
-static int lp503x_write_channels(const struct device *dev,
+static int lp5009_write_channels(const struct device *dev,
 				 uint32_t start_channel,
 				 uint32_t num_channels, const uint8_t *buf)
 {
 	const struct lp50xx_config *config = dev->config;
 	struct lp50xx_data *data = dev->data;
 
-	if (start_channel >= LP503X_NUM_CHANNELS ||
-	    start_channel + num_channels > LP503X_NUM_CHANNELS) {
+	if (start_channel >= LP5009_NUM_CHANNELS ||
+	    start_channel + num_channels > LP5009_NUM_CHANNELS) {
 		return -EINVAL;
 	}
 
@@ -100,23 +86,23 @@ static int lp503x_write_channels(const struct device *dev,
 	 * Unfortunately this controller don't support commands split into
 	 * two I2C messages.
 	 */
-	data->chan_buf[0] = LP503X_CHANNEL_BASE + start_channel;
+	data->chan_buf[0] = LP5009_CHANNEL_BASE + start_channel;
 	memcpy(data->chan_buf + 1, buf, num_channels);
 
 	return i2c_write_dt(&config->bus, data->chan_buf, num_channels + 1);
 }
 
-static int lp503x_on(const struct device *dev, uint32_t led)
+static int lp5009_on(const struct device *dev, uint32_t led)
 {
-	return lp503x_set_brightness(dev, led, 100);
+	return lp5009_set_brightness(dev, led, 100);
 }
 
-static int lp503x_off(const struct device *dev, uint32_t led)
+static int lp5009_off(const struct device *dev, uint32_t led)
 {
-	return lp503x_set_brightness(dev, led, 0);
+	return lp5009_set_brightness(dev, led, 0);
 }
 
-static int lp503x_init(const struct device *dev)
+static int lp5009_init(const struct device *dev)
 {
 	const struct lp50xx_config *config = dev->config;
 	uint8_t buf[3];
@@ -127,9 +113,9 @@ static int lp503x_init(const struct device *dev)
 		return -ENODEV;
 	}
 
-	if (config->num_leds > LP503X_MAX_LEDS) {
+	if (config->num_leds > LP5009_MAX_LEDS) {
 		LOG_ERR("%s: invalid number of LEDs %d (max %d)",
-			dev->name, config->num_leds, LP503X_MAX_LEDS);
+			dev->name, config->num_leds, LP5009_MAX_LEDS);
 		return -EINVAL;
 	}
 
@@ -147,7 +133,7 @@ static int lp503x_init(const struct device *dev)
 	 */
 
 	/* Reset device */
-	buf[0] = LP503X_DEVICE_RESET;
+	buf[0] = LP5009_DEVICE_RESET;
 	buf[1] = 0xff;
 	err = i2c_write_dt(&config->bus, buf, 2);
 	if (err < 0) {
@@ -177,13 +163,13 @@ static int lp503x_init(const struct device *dev)
 	return i2c_write_dt(&config->bus, buf, 2);
 }
 
-static const struct led_driver_api lp503x_led_api = {
-	.on		= lp503x_on,
-	.off		= lp503x_off,
+static const struct led_driver_api lp5009_led_api = {
+	.on		= lp5009_on,
+	.off		= lp5009_off,
 	.get_info	= lp50xx_get_info,
-	.set_brightness	= lp503x_set_brightness,
-	.set_color	= lp503x_set_color,
-	.write_channels	= lp503x_write_channels,
+	.set_brightness	= lp5009_set_brightness,
+	.set_color	= lp5009_set_color,
+	.write_channels	= lp5009_write_channels,
 };
 
 #define COLOR_MAPPING(led_node_id)					\
@@ -199,35 +185,35 @@ const uint8_t color_mapping_##led_node_id[] =				\
 	.color_mapping	= color_mapping_##led_node_id,			\
 },
 
-#define LP503x_DEVICE(id)						\
+#define LP5009_DEVICE(id)						\
 									\
 DT_INST_FOREACH_CHILD(id, COLOR_MAPPING)				\
 									\
-static const struct led_info lp503x_leds_##id[] = {			\
+static const struct led_info lp5009_leds_##id[] = {			\
 	DT_INST_FOREACH_CHILD(id, LED_INFO)				\
 };									\
 									\
-static uint8_t lp503x_chan_buf_##id[LP503X_NUM_CHANNELS + 1];		\
+static uint8_t lp5009_chan_buf_##id[LP5009_NUM_CHANNELS + 1];		\
 									\
 static struct lp50xx_config lp50xx_config_##id = {			\
 	.bus		= I2C_DT_SPEC_INST_GET(id),			\
-	.num_leds	= ARRAY_SIZE(lp503x_leds_##id),			\
+	.num_leds	= ARRAY_SIZE(lp5009_leds_##id),			\
 	.max_curr_opt	= DT_INST_PROP(id, max_curr_opt),		\
 	.log_scale_en	= DT_INST_PROP(id, log_scale_en),		\
-	.leds_info	= lp503x_leds_##id,				\
+	.leds_info	= lp5009_leds_##id,				\
 	.en_pin		= GPIO_DT_SPEC_INST_GET_OR(id, en_gpios, {0}),	\
 };									\
 									\
 static struct lp50xx_data lp50xx_data_##id = {				\
-	.chan_buf	= lp503x_chan_buf_##id,				\
+	.chan_buf	= lp5009_chan_buf_##id,				\
 };									\
 									\
 DEVICE_DT_INST_DEFINE(id,						\
-		    &lp503x_init,					\
+		    &lp5009_init,					\
 		    NULL,						\
 		    &lp50xx_data_##id,					\
 		    &lp50xx_config_##id,				\
 		    POST_KERNEL, CONFIG_LED_INIT_PRIORITY,		\
-		    &lp503x_led_api);
+		    &lp5009_led_api);
 
-DT_INST_FOREACH_STATUS_OKAY(LP503x_DEVICE)
+DT_INST_FOREACH_STATUS_OKAY(LP5009_DEVICE)
---
+++ drivers/led/lp50xx.c
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2020 Seagate Technology LLC
+ * Copyright (c) 2022 Grinn
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief LP50xx LED controller
+ */
+
+#include "lp50xx.h"
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/device.h>
+#include <zephyr/zephyr.h>
+
+const struct led_info *lp50xx_led_to_info(const struct lp50xx_config *config, uint32_t led)
+{
+	for (uint8_t i = 0; i < config->num_leds; i++) {
+		if (config->leds_info[i].index == led) {
+			return &config->leds_info[i];
+		}
+	}
+	return NULL;
+}
+
+int lp50xx_get_info(const struct device *dev, uint32_t led, const struct led_info **info)
+{
+	const struct lp50xx_config *config = dev->config;
+	const struct led_info *led_info = lp50xx_led_to_info(config, led);
+
+	if (!led_info) {
+		return -EINVAL;
+	}
+
+	*info = led_info;
+
+	return 0;
+}
